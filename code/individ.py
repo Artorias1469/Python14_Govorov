1208ed76e68089b60fb86cc4b17356da3479a186
Artorias1469 <pupas5676@gmail.com>
2023-12-23 21:56:04 +0300

    add

---
+++ code/individ.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def create_greeting(template):
+    def inner_function(last_name, first_name):
+        formatted_template = template.replace("%F%", last_name).replace("%N%", first_name)
+        return formatted_template
+
+    return inner_function
+
+greet_template = "Уважаемый %F%, %N%! Вы делаете работу по замыканиям функций."
+greet_function = create_greeting(greet_template)
+
+if __name__ == "__main__":
+    last_name = input("Введите фамилию: ")
+    first_name = input("Введите имя: ")
+
+    result = greet_function(last_name, first_name)
+    print(result)
